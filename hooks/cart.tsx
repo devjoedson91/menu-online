d4500067972afcb4135acba4fa33aebbd59ba42c
Joedson Ferreira <95484503+devjoedson91@users.noreply.github.com>
2024-05-10 14:16:49 -0300

    fix - localstorage not defined

--- hooks/cart.tsx
+++ hooks/cart.tsx
@@ -8,103 +8,88 @@ type CartProviderProps = {
 };
 
 type CartContextData = {
-    cart: Product[];
-    addProduct: (productId: number) => Promise<void>;
-    removeProduct: (productId: number) => void;
-    // updateProductAmount: ({ productId, amount }: UpdateProductAmount) => void;
+  cart: Product[];
+  addProduct: (productId: number) => Promise<void>;
+  removeProduct: (productId: number) => void;
+  // updateProductAmount: ({ productId, amount }: UpdateProductAmount) => void;
 };
 
 export const Cart = createContext({} as CartContextData);
 
 export function CartProvider({ children }: CartProviderProps) {
-
   const { toast } = useToast();
 
   const [cart, setCart] = useState<Product[]>(() => {
-
-    const storagedCart = localStorage.getItem('@burger:cart');
+    const storagedCart =
+      typeof window !== "undefined"
+        ? localStorage.getItem("@burger:cart")
+        : false;
 
     if (storagedCart) {
       return JSON.parse(storagedCart);
     }
 
     return [];
-
   });
 
-  const prevCartRef = useRef<Product[]>()
+  const prevCartRef = useRef<Product[]>();
 
   useEffect(() => {
-
-      prevCartRef.current = cart;
-
+    prevCartRef.current = cart;
   });
 
   const cartPreviousValue = prevCartRef.current ?? cart;
 
   useEffect(() => {
-
     if (cartPreviousValue !== cart) {
-      localStorage.setItem('@burger:cart', JSON.stringify(cart));
+      localStorage.setItem("@burger:cart", JSON.stringify(cart));
     }
   }, [cart, cartPreviousValue]);
 
   const addProduct = async (productId: number) => {
-
     try {
-
       const updateCart = [...cart]; // toda alteração no updateCart não vai refletir no cart
-      const productExists = updateCart.find(product => product.id === productId);
+      const productExists = updateCart.find(
+        (product) => product.id === productId,
+      );
 
       const currentAmount = productExists ? productExists.amount : 0;
       const amount = currentAmount + 1; // quantidade desejada
 
-
       if (productExists) {
-          productExists.amount = amount;
+        productExists.amount = amount;
       } else {
-        
         //   const product = await api.get(`/products/${productId}`)
-
         //   const newProduct = {
         //     ...product.data,
         //     amount: 1
         //   }
-
         //   updateCart.push(newProduct);
       }
 
       setCart(updateCart);
-
-    } catch(err) {
-        
-        toast({description: 'Erro na adição do produto'});
+    } catch (err) {
+      toast({ description: "Erro na adição do produto" });
     }
-
   };
 
   const removeProduct = (productId: number) => {
     try {
       const updateCart = [...cart];
-      const indexItem = updateCart.findIndex(product => product.id === productId);
-      
+      const indexItem = updateCart.findIndex(
+        (product) => product.id === productId,
+      );
+
       if (indexItem >= 0) {
-
-          updateCart.splice(indexItem, 1);
-          setCart(updateCart);
-          
+        updateCart.splice(indexItem, 1);
+        setCart(updateCart);
       } else {
-
-          throw Error();
-
+        throw Error();
       }
-
-    } catch(err) {
-      
-      toast({description: 'Erro na remoção do produto'});
+    } catch (err) {
+      toast({ description: "Erro na remoção do produto" });
     }
   };
-
 
   return (
     <Cart.Provider value={{ cart, addProduct, removeProduct }}>
