2ddf1964a7f66435289a64609268b3bc77e9ef1a
Joedson Ferreira <95484503+devjoedson91@users.noreply.github.com>
2024-05-10 13:51:56 -0300

    chora - add script postinstall

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,9 @@
 import Footer from "@/components/ui/footer";
 import Header from "@/components/ui/header";
 import ProductCard from "@/components/ui/productCard";
-import Refri1 from "@/public/refri-1.png";
-import Refri2 from "@/public/refri-2.png";
 import { prismaClient } from "@/lib/prisma";
 
 export default async function Home() {
-
   const products = await prismaClient.product.findMany({
     where: {
       categoryId: "78869090-6c67-4533-bee0-26fa6f5c0f40",
@@ -24,38 +21,39 @@ export default async function Home() {
   return (
     <>
       <Header />
-      <h2 className="capitalize md:text-3xl text-2xl font-bold text-center mt-9 mb-6">conheça nosso menu</h2>
+      <h2 className="mb-6 mt-9 text-center text-2xl font-bold capitalize md:text-3xl">
+        conheça nosso menu
+      </h2>
       <div>
-        <main className="grid grid-cols-1 md:grid-cols-2 gap-7 md:gap-10 mx-auto max-w-7xl px-2 mb-16">
-          {
-            products.slice(0, 4).map(product => {
-
-              return (
-                <ProductCard 
-                  key={product.id} 
-                  image_url={product.imageUrl} 
-                  name={product.name} 
-                  price={product.price} 
-                  description={product.description}  
-                />
-              )
-            })
-          }
+        <main className="mx-auto mb-16 grid max-w-7xl grid-cols-1 gap-7 px-2 md:grid-cols-2 md:gap-10">
+          {products.slice(0, 4).map((product) => {
+            return (
+              <ProductCard
+                key={product.id}
+                image_url={product.imageUrl}
+                name={product.name}
+                price={product.price}
+                description={product.description}
+              />
+            );
+          })}
         </main>
 
-        <div className="mx-auto max-w-7xl px-2 my-2">
-          <h2 className="font-bold text-3xl">Bebidas</h2>
+        <div className="mx-auto my-2 max-w-7xl px-2">
+          <h2 className="text-3xl font-bold">Bebidas</h2>
         </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-7 md:gap-10 mx-auto max-w-7xl px-2 mb-16">
-          {
-            drinks.slice(0, 6).map(drink => {
-              return (
-                <ProductCard key={drink.id} image_url={drink.imageUrl} price={drink.price} name={drink.name} />
-              )
-            })
-          }
-
+        <div className="mx-auto mb-16 grid max-w-7xl grid-cols-1 gap-7 px-2 md:grid-cols-2 md:gap-10">
+          {drinks.slice(0, 6).map((drink) => {
+            return (
+              <ProductCard
+                key={drink.id}
+                image_url={drink.imageUrl}
+                price={drink.price}
+                name={drink.name}
+              />
+            );
+          })}
         </div>
       </div>
       <Footer />
